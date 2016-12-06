987a80df15c66254fc3c57649d5d602c3324e4bf
Jonathan Daniel <jonathandaniel@fernbanklinks.com>
2016-12-05 21:13:08 -0500

    Contour Drawing Working
    Now the program can draw the contours. Now, we just need to work on
    filtering.

--- main.py
+++ main.py
@@ -15,14 +15,20 @@ while True:
     #filters the colors
     mask                = cv2.inRange(hsv, lower_green, upper_green)
     res                 = cv2.bitwise_and(frame,frame, mask= mask)
-    #converts the result to grayscale for the threshold and the contours (may remove this)
-    gray                = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
-    #does a binary threshold of the grayscale (not being used now)
-    _, thres            = cv2.threshold(gray,127,255,cv2.THRESH_BINARY)
-    #finds the contours of the grayscale
-    _, contours, _ = cv2.findContours(gray,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    #finds the contours of the picture
+    _, contours, _ = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     #draws the contours to the screen
     cv2.drawContours(res,contours,-1,(0,255,0),3)
+
+    #Need this otherwise the program will crash
+    if len(contours) >= 1:
+        cnt = contours[0]
+        M = cv2.moments(cnt)
+        #Also needed, otherwise the program will crash
+        if M["m00"] != 0:
+            cx = int(M['m10']/M['m00'])
+            cy = int(M['m01']/M['m00'])
+            cv2.circle(res, (cx, cx), 7, (255, 255, 255), -1)
     #displays the video
     cv2.imshow('image',res)
     if cv2.waitKey(5) & 0xFF == 27: break
