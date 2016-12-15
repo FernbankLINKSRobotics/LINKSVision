31ca4ba33818559ab7bc1bbc862154bb27f7998a
Jonathan Daniel <jonathandaniel@fernbanklinks.com>
2016-12-14 19:50:32 -0500

    Added Real HSV FIltering along with Exposure Setting.
    So, now the program takes the exposure doen to the lowest level and we
    found good values for filtering. Also, the exposureTest file is what we
    used to test the code.

--- main.py
+++ main.py
@@ -3,17 +3,15 @@ import numpy as np
 from networktables import NetworkTable
 
 
-debug = False
+debug = True
 largestArea = 0
+cx, cy = 0, 0
 
 #Set up NetworkTables
-NetworkTable.setIPAddress("10.44.68.2")
-NetworkTable.setClientMode()
-NetworkTable.initialize()
-table = NetworkTable.getTable("LINKSVision")
 
 #This is the actual video stream
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(2)
+cap.set(cv2.CAP_PROP_EXPOSURE, -1.0)
 while True:
     #reads in the video and breaks it into frames
     running, frame            = cap.read()
@@ -26,8 +24,8 @@ while True:
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
         #The Ranges of Green Color
-        lower_green = np.array([60,175,140])
-        upper_green = np.array([180,255,255])
+        lower_green = np.array([70,50,50])
+        upper_green = np.array([90,255,255])
 
         #Now, we need to find out what is in that range
         #OpenCV will return a binary image where white = 1
@@ -35,7 +33,7 @@ while True:
         mask = cv2.inRange(hsv, lower_green, upper_green)
 
         #The Kernel is just an array that is scanned over the
-        #image. It is full of ones.
+        #image. It is full of ones so it doesn't change anything.
         kernel = np.ones((5,5), np.uint8)
 
         #Now, we need to remove more noise! This can be done
@@ -56,20 +54,21 @@ while True:
         #Needed otherwise the program will crash
         if len(contours) >= 1:
             for cnt in contours:
-                area = cv2.countourArea(cnt)
-                if area > largestArea:
+                area = cv2.contourArea(cnt)
+                if area >= largestArea :
                     largestArea = area
                     M = cv2.moments(cnt)
                     #Also needed unless you like errors
                     if M["m00"] != 0:
                         cx = int(M['m10']/M['m00'])
                         cy = int(M['m01']/M['m00'])
-            #So, now that we have our working contour,
-            #All we need to do is send the data to the robot
-            table.putNumber("Center X", cx)
-            table.putNumber("Center Y", cy)
-            table.putNumber("Area ", largestArea)
+                        print("Center X: %s" %(cx))
+                        print("Center Y: %s" %(cy))
+                    
             
 
         if debug:
-            cv2.imshow("Image", res)
+            cv2.drawContours(res, contours, -1, (0, 255, 0), 3)
+            cv2.imshow("Initial", frame)
+            cv2.imshow("Final", res)
+        if cv2.waitKey(5) & 0xFF == 27: break
