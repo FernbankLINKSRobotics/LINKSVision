8570f4185c940ad2826cbfb38ff056648b5c3eba
Jonathan Daniel <jonathandaniel@fernbanklinks.com>
2017-01-08 09:58:12 -0500

    Initial Commit for STEAMWORKS
    So, this version should find the two largest contours and average out
    the center X and center Y of both. Thus giving us a good target.

--- main.py
+++ main.py
@@ -1,29 +1,22 @@
 import cv2
 import numpy as np
 
-
-
 largestArea = 0
 cx, cy = 0, 0
 
-
 #DEBUG Variables
 debug = True #Do we want to see an image output?
-networkTables = True #Are we testing out the Network Tables?
-
 
 filterArea = 1000
 
-#Set up NetworkTables
-
-
-
+firstContour = None
+secondContour = None
 
 cap = cv2.VideoCapture(1)
 cap.set(cv2.CAP_PROP_EXPOSURE, 1)
 
 while True:
-    #reads in the video and breaks it into frames
+    #Breaks the Camera Stream into Frames
     running, frame            = cap.read()
 
     if running:
@@ -62,47 +55,57 @@ while True:
         _, contours, _ = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
         #Needed otherwise the program will crash
-        if len(contours) >= 1:
+        if len(contours) >= 2:
             largestArea = 0
+            secondLargestArea = 0
+
+            #So, this is an array of all of the contour
+            #areas, and they are added to this array
+            #from the main contour array. This is done
+            #so we can find the largest and second largest
+            #contours as those will be the tape
+            areas = []
             for cnt in contours:
-                area = cv2.contourArea(cnt)
-                if area >= largestArea * .8 and area >= filterArea:
+                areas.append(cv2.contourArea(cnt))
+
+            for area in areas:
+                if area > largestArea:
+                    #Now, we find the largest area
                     largestArea = area
-                    M = cv2.moments(cnt)
-                    #Also needed unless you like errors
-                    if M["m00"] != 0:
-                        cx = int(M['m10']/M['m00'])
-                        cy = int(M['m01']/M['m00'])
-                        print("Center X: %s" %(cx))
-                        print("Center Y: %s" %(cy))
-                    else:
-                        cx, cy = 0, 0                      
 
-                    if debug:
-                        #For visualization, it would be nice to see what the robot
-                        #considers the largest rectangle to be. Time to introduce
-                        #hulls & boxes!
+            #and get the index of it from the array
+            indexOfLargest = areas.index(area)
+            cnt1 = contours[indexOfLargest]
             
-                        #First, we tell the robot to look for the convex points around
-                        #the contour.
-                        hull = cv2.convexHull(cnt)
-                        #With these points, we now are looking for the smallest
-                        #rectangle around said points
-                        rect = cv2.minAreaRect(hull)
-                        #Unfortantly, minAreaRect() only gets the center (x,y),
-                        #(width, height), angle of rotation. To get
-                        #around this, we use the boxpoints function to get the
-                        #corners of the rectangle.
-                        box = cv2.boxPoints(rect)
-                        #With the corners, now we 
-                        box = np.int0(box)
+            for area in areas:
+                #So, finding the second largest is a little
+                #tricky. What I think we should do is iterate
+                #through the array again, but if we are at the
+                #largest area, we skip over it.
+                if area == areas[indexOfLargest]:
+                    continue
+                #and keep going until we find the second largest
+                if area > secondLargestArea:
+                    secondLargestArea = area
+            
+            indexOfSecondLargest = areas.index(area)
+            cnt2 = contours[indexOfSecondLargest]
 
-                        #The Circle will be used as a reference of what
-                        #the contour's center is
-                        cv2.circle(res,(cx, cy), 8, (0,0,255), -1)
-                        
-                        
-                    
+            #Get the Moments of each contour
+            M1 = cv2.moments(cnt1)
+            M2 = cv2.moments(cnt2)
+
+            if M1["m00"] != 0 and M2["m00"] != 0:
+                #Average Center X
+                cx1 = int(M1['m10']/M1['m00'])
+                cx2 = int(M2['m10']/M2['m00'])
+                cx = (cx1 + cx2) / 2.0
+
+                #Average Center Y
+                cy1 = int(M1['m01']/M1['m00'])
+                cy2 = int(M2['m01']/M2['m00'])
+                cy = (cy1 + cy2) / 2.0
+                
         if debug:
             cv2.drawContours(res, contours, -1, (0, 255, 0), 3)
             cv2.circle(res,(cx, cy), 8, (0,0,255), -1)
